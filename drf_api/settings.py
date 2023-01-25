d9dd2aae46978d2d9eb1be91279e4873df13f2a6
Rhesmika <Rhesmika@gmail.com>
2023-01-25 20:13:09 +0000

    update allowed hosts

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -58,7 +58,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEV' in os.environ
 
-ALLOWED_HOSTS = ['localhost', '<drf-api-rh>.herokuapp.com']
+ALLOWED_HOSTS = ['localhost', 'drf-api-rh.herokuapp.com']
 
 
 # Application definition
